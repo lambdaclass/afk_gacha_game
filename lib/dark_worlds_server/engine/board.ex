43b3944db815bb4c35db8866b9e17c6b638f75fd
Mati Onorato <onoratomatias@gmail.com>
2023-04-11 03:07:35 +0200

    add player

--- lib/dark_worlds_server/engine/board.ex
+++ lib/dark_worlds_server/engine/board.ex
@@ -13,11 +13,19 @@ defmodule DarkWorldsServer.Engine.Board do
     %__MODULE__{grid: grid}
   end
 
-  def add_player(%__MODULE__{} = board, %Player{} = player) do
-    rand_y = Enum.random(1,get_height(board))
-    rand_x = Enum.random(1,get_width(board))
+  def new(grid) do
+    %__MODULE__{grid: grid}
+  end
 
+  def add_player(%__MODULE__{grid: grid} = board, %Player{number: number} = _player) do
+    rand_y = Enum.random(0..get_height(board)-1)
+    rand_x = Enum.random(0..get_width(board)-1)
 
+    IO.inspect({rand_y,rand_x})
+
+    grid
+    |> Nx.put_slice([rand_y,rand_x], Nx.tensor([[number]]))
+    |> new()
   end
 
   def get_height(%__MODULE__{} = board) do
