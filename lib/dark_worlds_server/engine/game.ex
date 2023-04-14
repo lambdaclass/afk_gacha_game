3fe1e3e323546c76b4f8b80d55e899935166db4d
Javier Chatruc <jrchatruc@gmail.com>
2023-04-14 13:05:11 -0300

    Address comment, remove unused function

--- lib/dark_worlds_server/engine/game.ex
+++ lib/dark_worlds_server/engine/game.ex
@@ -1,5 +1,6 @@
 defmodule DarkWorldsServer.Engine.Game do
   alias DarkWorldsServer.Engine.{Board, Player}
+  use Rustler, otp_app: :dark_worlds_server, crate: "gamestate"
 
   @enforce_keys [:players, :board]
   defstruct [:players, :board]
@@ -8,18 +9,6 @@ defmodule DarkWorldsServer.Engine.Game do
     new_game(number_of_players, width, height)
   end
 
-  use Rustler, otp_app: :dark_worlds_server, crate: "gamestate"
-
   def new_game(_a, _b, _c), do: :erlang.nif_error(:nif_not_loaded)
   def move_player(_a, _b, _c), do: :erlang.nif_error(:nif_not_loaded)
-
-  defp update_player(players, %Player{} = player) do
-    Enum.map(players, fn p ->
-      if p.number == player.number do
-        player
-      else
-        p
-      end
-    end)
-  end
 end
