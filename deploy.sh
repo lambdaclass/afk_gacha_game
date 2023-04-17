835ac45fb8c6bff5a896c23453814248abaa58b7
Klaus Lungwitz <18153834+klaus993@users.noreply.github.com>
2023-04-17 18:22:59 -0300

    Revert "XXX: Trigger deploy"
    This reverts commit 6790e8ff8ea6170c4e9f098706c158982bbe3ec8.

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ if [ -d "/tmp/dark_worlds_server" ]; then
 fi
 
 cd /tmp
-git clone git@github.com:lambdaclass/dark_worlds_server.git --branch deploy-script
+git clone git@github.com:lambdaclass/dark_worlds_server.git --branch main
 cd dark_worlds_server
 
 mix local.hex --force && mix local.rebar --force
