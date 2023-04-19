185db381b7a99050586c4d088e9dec8b7c167978
tvillegas98 <tvillegas@fi.uba.ar>
2023-04-19 17:34:13 -0300

    Add game timeout and session timeout

--- lib/dark_worlds_server/engine/runner.ex
+++ lib/dark_worlds_server/engine/runner.ex
@@ -6,18 +6,24 @@ defmodule DarkWorldsServer.Engine.Runner do
 
   @players 2
   @board {10, 10}
-  # 5 minutes in milliseconds
-  @session_timeout_ms 5 * 60 * 1000
+  # The game will be closed five minute after it starts
+  @game_timeout 5 * 60 * 1000
+  # The session will be closed one minute after the game has finished
+  @session_timeout 60 * 1000
 
   def start_link(args) do
     GenServer.start_link(__MODULE__, args)
   end
 
   def init(_opts) do
-    state = Game.new(number_of_players: @players, board: @board)
-    IO.inspect(state)
+    game = Game.new(number_of_players: @players, board: @board)
+
+    IO.inspect(game)
     IO.inspect("To join: #{encode_pid(self())}")
-    {:ok, state, @session_timeout_ms}
+
+    Process.send_after(self(), :game_timeout, @game_timeout)
+
+    {:ok, %{game: game, has_finished?: false}}
   end
 
   def play(runner_pid, %ActionOk{} = action) do
@@ -32,42 +38,54 @@ defmodule DarkWorldsServer.Engine.Runner do
     GenServer.call(runner_pid, :get_players)
   end
 
-  def handle_cast({:play, %ActionOk{action: :move, player: player, value: value}}, state) do
-    state =
-      state
+  def handle_cast(
+        {:play, %ActionOk{action: :move, player: player, value: value}},
+        %{game: game} = state
+      ) do
+    game =
+      game
       |> Game.move_player(player, value)
 
     DarkWorldsServer.PubSub
     |> Phoenix.PubSub.broadcast("game_play_#{encode_pid(self())}", {:move, state.board})
 
-    {:noreply, state, @session_timeout_ms}
+    {:noreply, Map.put(state, :game, game)}
   end
 
-  def handle_cast({:play, %ActionOk{action: :attack, player: player, value: value}}, state) do
-    state =
-      state
+  def handle_cast(
+        {:play, %ActionOk{action: :attack, player: player, value: value}},
+        %{game: game} = state
+      ) do
+    game =
+      game
       |> Game.attack_player(player, value)
 
     DarkWorldsServer.PubSub
     |> Phoenix.PubSub.broadcast("game_play_#{encode_pid(self())}", {:attack, state.players})
 
-    {:noreply, state}
+    {:noreply, Map.put(state, :game, game)}
   end
 
-  def handle_call(:get_board, _from, %Game{board: board} = state) do
-    {:reply, board, state, @session_timeout_ms}
+  def handle_call(:get_board, _from, %{game: %Game{board: board}} = state) do
+    {:reply, board, state}
   end
 
-  def handle_call(:get_players, _from, %Game{players: players} = state) do
+  def handle_call(:get_players, _from, %{game: %Game{players: players}} = state) do
     {:reply, players, state}
   end
 
-  def handle_info(:timeout, state) do
+  def handle_info(:game_timeout, state) do
     IO.inspect(self(), label: "session timeout")
-    {:stop, :normal, state}
+
+    {:noreply, Map.put(state, :has_finished?, true), @session_timeout}
   end
 
   def encode_pid(pid) do
     pid |> :erlang.term_to_binary() |> Base.encode64()
+  end
+
+  def game_has_finished?(pid) do
+    %{has_finished?: has_finished?} = :sys.get_state(pid)
+    has_finished?
   end
 end
