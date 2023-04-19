1537f5f1d924fd8ebe48c050af2d0b8026cceed2
Javier Chatruc <jrchatruc@gmail.com>
2023-04-19 10:56:15 -0300

    Use migrate script when migrating on CI

--- deploy.sh
+++ deploy.sh
@@ -52,7 +52,7 @@ EOF
 
 systemctl stop dark_worlds_server
 
-/root/dark_worlds_server/_build/prod/rel/dark_worlds_server/bin/dark_worlds_server migrate
+/root/dark_worlds_server/_build/prod/rel/dark_worlds_server/bin/migrate
 
 systemctl daemon-reload
 systemctl start dark_worlds_server
