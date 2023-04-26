49955467e6afa142694ab874c73361297e7fd25d
Javier Chatruc <jrchatruc@gmail.com>
2023-04-26 15:11:07 -0300

    Add tuple encoding so tiles can now be properly encoded in JSON

--- lib/dark_worlds_server/engine/board.ex
+++ lib/dark_worlds_server/engine/board.ex
@@ -2,4 +2,16 @@ defmodule DarkWorldsServer.Engine.Board do
   @enforce_keys [:grid]
   @derive Jason.Encoder
   defstruct [:grid]
+
+  defmodule TupleEncoder do
+    alias Jason.Encoder
+
+    defimpl Encoder, for: Tuple do
+      def encode(data, options) when is_tuple(data) do
+        data
+        |> Tuple.to_list()
+        |> Encoder.List.encode(options)
+      end
+    end
+  end
 end
