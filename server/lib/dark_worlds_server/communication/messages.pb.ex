59e78802ae7c60410f82b61ff0169432335302c7
Amin Arria <arria.amin@gmail.com>
2023-05-03 21:13:19 +0200

    Messages for incoming actions

--- server/lib/dark_worlds_server/communication/messages.pb.ex
+++ server/lib/dark_worlds_server/communication/messages.pb.ex
@@ -1,3 +1,27 @@
+defmodule DarkWorldsServer.Communication.Proto.Action do
+  @moduledoc false
+
+  use Protobuf, enum: true, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field :ACTION_UNSPECIFIED, 0
+  field :MOVE, 1
+  field :ATTACK, 2
+  field :PING, 3
+  field :UPDATE_PING, 4
+end
+
+defmodule DarkWorldsServer.Communication.Proto.Direction do
+  @moduledoc false
+
+  use Protobuf, enum: true, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field :DIRECTION_UNSPECIFIED, 0
+  field :UP, 1
+  field :DOWN, 2
+  field :LEFT, 3
+  field :RIGHT, 4
+end
+
 defmodule DarkWorldsServer.Communication.Proto.GameStateUpdate do
   @moduledoc false
 
@@ -28,6 +52,18 @@ defmodule DarkWorldsServer.Communication.Proto.Position do
 
   field :x, 1, type: :uint32
   field :y, 2, type: :uint32
+
+  def transform_module(), do: DarkWorldsServer.Communication.ProtoTransform
+end
+
+defmodule DarkWorldsServer.Communication.Proto.ClientAction do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field :action, 1, type: DarkWorldsServer.Communication.Proto.Action, enum: true
+  field :direction, 2, type: DarkWorldsServer.Communication.Proto.Direction, enum: true
+  field :latency, 3, type: :uint32
 
   def transform_module(), do: DarkWorldsServer.Communication.ProtoTransform
 end
--- server/lib/dark_worlds_server/communication/proto_transform.ex
+++ server/lib/dark_worlds_server/communication/proto_transform.ex
@@ -1,9 +1,11 @@
 defmodule DarkWorldsServer.Communication.ProtoTransform do
   alias DarkWorldsServer.Engine.Position, as: EnginePosition
   alias DarkWorldsServer.Engine.Player, as: EnginePlayer
+  alias DarkWorldsServer.Engine.ActionOk, as: EngineAction
   alias DarkWorldsServer.Communication.Proto.Position, as: ProtoPosition
   alias DarkWorldsServer.Communication.Proto.Player, as: ProtoPlayer
   alias DarkWorldsServer.Communication.Proto.GameStateUpdate
+  alias DarkWorldsServer.Communication.Proto.ClientAction, as: ProtoAction
 
   @behaviour Protobuf.TransformModule
 
@@ -22,6 +24,22 @@ defmodule DarkWorldsServer.Communication.ProtoTransform do
     %GameStateUpdate{players: players}
   end
 
+  def encode(%EngineAction{action: :move, value: direction}, ProtoAction) do
+    %ProtoAction{action: :MOVE, direction: direction_encode(direction)}
+  end
+
+  def encode(%EngineAction{action: :attack, value: direction}, ProtoAction) do
+    %ProtoAction{action: :ATTACK, direction: direction_encode(direction)}
+  end
+
+  def encode(%EngineAction{action: :ping}, ProtoAction) do
+    %ProtoAction{action: :PING}
+  end
+
+  def encode(%EngineAction{action: :update_ping, value: latency}, ProtoAction) do
+    %ProtoAction{action: :UPDATE_PING, latency: latency}
+  end
+
   @impl Protobuf.TransformModule
   def decode(%ProtoPosition{} = position, ProtoPosition) do
     %{x: x, y: y} = position
@@ -36,4 +54,33 @@ defmodule DarkWorldsServer.Communication.ProtoTransform do
   def decode(%GameStateUpdate{players: players}, GameStateUpdate) do
     %{players: players}
   end
+
+  def decode(%ProtoAction{action: :MOVE, direction: direction}, ProtoAction) do
+    %EngineAction{action: :move, value: direction_decode(direction)}
+  end
+
+  def decode(%ProtoAction{action: :ATTACK, direction: direction}, ProtoAction) do
+    %EngineAction{action: :attack, value: direction_decode(direction)}
+  end
+
+  def decode(%ProtoAction{action: :PING}, ProtoAction) do
+    %EngineAction{action: :ping, value: :ping}
+  end
+
+  def decode(%ProtoAction{action: :UPDATE_PING, latency: latency}, ProtoAction) do
+    %EngineAction{action: :update_ping, value: latency}
+  end
+
+  ###############################
+  # Helpers for transformations #
+  ###############################
+  defp direction_encode(:up), do: :UP
+  defp direction_encode(:down), do: :DOWN
+  defp direction_encode(:left), do: :LEFT
+  defp direction_encode(:right), do: :RIGHT
+
+  defp direction_decode(:UP), do: :up
+  defp direction_decode(:DOWN), do: :down
+  defp direction_decode(:LEFT), do: :left
+  defp direction_decode(:RIGHT), do: :right
 end
