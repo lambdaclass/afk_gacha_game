cde049040a342c31daa3dd226ce97e421bd3f15d
Martina <martina@martina>
2023-05-04 21:20:05 -0300

    fix typo

--- server/lib/dark_worlds_server_web/router.ex
+++ server/lib/dark_worlds_server_web/router.ex
@@ -5,7 +5,7 @@ defmodule DarkWorldsServerWeb.Router do
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
-    plug :fetch_live_flash0
+    plug :fetch_live_flash
     plug :put_root_layout, {DarkWorldsServerWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
