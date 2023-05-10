aba5bb35ee98a5b55789795ea7a0db4edc978c42
Francisco Krause Arnim <fkrausear@gmail.com>
2023-05-10 13:32:15 -0300

    Server: Remove unnecessary clause in mix file

--- server/mix.exs
+++ server/mix.exs
@@ -27,7 +27,7 @@ defmodule DarkWorldsServer.MixProject do
   end
 
   # Specifies which paths to compile per environment.
-  defp elixirc_paths(env) when env in [:test], do: ["lib", "test/support"]
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
