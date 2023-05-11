80ee43f57bce85f355f28118aad8b56553d19353
Javier Chatruc <jrchatruc@gmail.com>
2023-05-11 12:17:24 -0300

    Remove messages.proto file from the load test project

--- server/load_test/lib/load_test/communication/messages.pb.ex
+++ server/load_test/lib/load_test/communication/messages.pb.ex
@@ -22,6 +22,43 @@ defmodule LoadTest.Communication.Proto.Direction do
   field :RIGHT, 4
 end
 
+defmodule LoadTest.Communication.Proto.GameStateUpdate do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field :players, 1, repeated: true, type: LoadTest.Communication.Proto.Player
+end
+
+defmodule LoadTest.Communication.Proto.Player do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field :id, 1, type: :uint32
+  field :health, 2, type: :uint32
+  field :position, 3, type: LoadTest.Communication.Proto.Position
+  field :power, 4, type: :uint32
+end
+
+defmodule LoadTest.Communication.Proto.Position do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field :x, 1, type: :uint32
+  field :y, 2, type: :uint32
+end
+
+defmodule LoadTest.Communication.Proto.UpdatePing do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field :player_id, 1, type: :uint32, json_name: "playerId"
+  field :latency, 2, type: :uint32
+end
+
 defmodule LoadTest.Communication.Proto.ClientAction do
   @moduledoc false
 
@@ -30,6 +67,4 @@ defmodule LoadTest.Communication.Proto.ClientAction do
   field :action, 1, type: LoadTest.Communication.Proto.Action, enum: true
   field :direction, 2, type: LoadTest.Communication.Proto.Direction, enum: true
   field :latency, 3, type: :uint32
-
-  def transform_module(), do: LoadTest.Communication.ProtoTransform
 end
