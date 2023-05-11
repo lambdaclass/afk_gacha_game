2706b28deb0166784e492e4731686595ce4617b4
Francisco Krause Arnim <fkrausear@gmail.com>
2023-05-11 19:09:37 -0300

    Update test

--- server/test/dark_worlds_server/disconnect_test.exs
+++ server/test/dark_worlds_server/disconnect_test.exs
@@ -3,7 +3,7 @@ defmodule DarkWorldsServer.Test.Disconnect do
   alias DarkWorldsServer.Engine.Runner
 
   setup do
-    {:ok, pid} = Runner.start_link(%{})
+    {:ok, pid} = Runner.start_link(%{players: []})
     for _ <- 1..3, do: Runner.join(pid)
     %{pid: pid}
   end
