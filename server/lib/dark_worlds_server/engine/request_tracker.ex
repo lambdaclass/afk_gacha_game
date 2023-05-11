3317abe722095e4438892c9120fd5d3ffe3f1808
Amin Arria <arria.amin@gmail.com>
2023-05-11 21:28:27 +0200

    Improve report helpers and cleanup

--- server/lib/dark_worlds_server/engine/request_tracker.ex
+++ server/lib/dark_worlds_server/engine/request_tracker.ex
@@ -8,36 +8,53 @@ defmodule DarkWorldsServer.Engine.RequestTracker do
     :ets.update_counter(:request_tracker, key, 1, {key, 0})
   end
 
-  def report() do
-    aggregate =
-      :ets.tab2list(:request_tracker)
-      |> Enum.reduce(%{total: 0, msgs_per_game: %{}}, fn {{game_pid, player_id}, count}, acc ->
-        acc
-        |> update_in([:total], fn val -> val + count end)
-        |> update_in([:msgs_per_game, game_pid], fn x ->
-          val = x || %{total: 0, msgs_per_player: %{}}
-          %{val | :total => val.total + count}
-        end)
-        |> put_in([:msgs_per_game, game_pid, :msgs_per_player, player_id], count)
+  def clear_table() do
+    :ets.delete_all_objects(:request_tracker)
+  end
+
+  def aggregate_table() do
+    :ets.tab2list(:request_tracker)
+    |> Enum.reduce(%{total: 0, msgs_per_game: %{}}, fn {{game_pid, player_id}, count}, acc ->
+      acc
+      |> update_in([:total], fn val -> val + count end)
+      |> update_in([:msgs_per_game, game_pid], fn x ->
+        val = x || %{total: 0, msgs_per_player: %{}}
+        %{val | :total => val.total + count}
       end)
+      |> put_in([:msgs_per_game, game_pid, :msgs_per_player, player_id], count)
+    end)
+  end
+
+  def report() do
+    report(:player)
+  end
+
+  def report(detail_level) do
+    aggregate = aggregate_table()
 
     IO.puts("Report of request tracking")
     IO.puts("--------------------------")
     IO.puts("total msgs: #{aggregate.total}")
     IO.puts("total games: #{length(Map.keys(aggregate.msgs_per_game))}")
-    IO.puts("\nDetails per game")
-    IO.puts("------------------")
 
-    Enum.each(aggregate.msgs_per_game, fn {game_pid,
-                                           %{total: total, msgs_per_player: msgs_per_player}} ->
-      IO.puts("#{:erlang.pid_to_list(game_pid)} =>")
-      IO.puts("   total msgs: #{total}")
-      IO.puts("   total players: #{length(Map.keys(msgs_per_player))}")
-      IO.puts("   msgs per player =>")
+    if detail_level in [:game, :player] do
+      IO.puts("\nDetails per game")
+      IO.puts("------------------")
 
-      Enum.each(msgs_per_player, fn {player_id, value} ->
-        IO.puts("       player #{player_id}, total msg: #{value}")
+      Enum.each(aggregate.msgs_per_game, fn {game_pid,
+                                             %{total: total, msgs_per_player: msgs_per_player}} ->
+        IO.puts("#{:erlang.pid_to_list(game_pid)} =>")
+        IO.puts("   total msgs: #{total}")
+        IO.puts("   total players: #{length(Map.keys(msgs_per_player))}")
+
+        if detail_level == :player do
+          IO.puts("   msgs per player =>")
+
+          Enum.each(msgs_per_player, fn {player_id, value} ->
+            IO.puts("       player #{player_id}, total msg: #{value}")
+          end)
+        end
       end)
-    end)
+    end
   end
 end
