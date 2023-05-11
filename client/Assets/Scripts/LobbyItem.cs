002d3699e0d2e8fbe11533f38913dbc112b1f921
NContinanza <nicolas.continanza@lambdaclass.com>
2023-05-11 12:11:33 -0300

    Add LobbyItem script

---
+++ client/Assets/Scripts/LobbyItem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyItem : MonoBehaviour
+{
+    [SerializeField] GameObject idContainer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setId(string id)
+    {
+        idContainer.GetComponent<Text>().text = id;
+    }
+}
