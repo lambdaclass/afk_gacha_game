e8e14693bd29e1580f1511b6063b8728a6df17ab
tvillegas98 <tvillegas@fi.uba.ar>
2023-05-12 16:55:22 -0300

    Update proto messages for c#

--- client/Assets/Scripts/Messages.pb.cs
+++ client/Assets/Scripts/Messages.pb.cs
@@ -38,7 +38,7 @@ public partial class Player : global::ProtoBuf.IExtensible
     public uint Power { get; set; }
 
     [global::ProtoBuf.ProtoMember(5, Name = @"action")]
-    public uint Action { get; set; }
+    public PlayerAction Action { get; set; }
 
 }
 
@@ -108,15 +108,6 @@ public enum Action
 }
 
 [global::ProtoBuf.ProtoContract()]
-public enum PlayerAction
-{
-    [global::ProtoBuf.ProtoEnum(Name = @"NOTHING")]
-    Nothing = 0,
-    [global::ProtoBuf.ProtoEnum(Name = @"ATTACKING")]
-    Attacking = 1,
-}
-
-[global::ProtoBuf.ProtoContract()]
 public enum Direction
 {
     [global::ProtoBuf.ProtoEnum(Name = @"DIRECTION_UNSPECIFIED")]
@@ -129,6 +120,15 @@ public enum Direction
     Left = 3,
     [global::ProtoBuf.ProtoEnum(Name = @"RIGHT")]
     Right = 4,
+}
+
+[global::ProtoBuf.ProtoContract()]
+public enum PlayerAction
+{
+    [global::ProtoBuf.ProtoEnum(Name = @"NOTHING")]
+    Nothing = 0,
+    [global::ProtoBuf.ProtoEnum(Name = @"ATTACKING")]
+    Attacking = 1,
 }
 
 #pragma warning restore CS0612, CS0618, CS1591, CS3021, IDE0079, IDE1006, RCS1036, RCS1057, RCS1085, RCS1192
