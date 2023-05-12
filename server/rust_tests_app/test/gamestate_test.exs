26b21cf80cea3c930eebf32008835d2701194bef
Francisco Krause Arnim <fkrausear@gmail.com>
2023-05-12 11:27:31 -0300

    Skip tests for now

--- server/rust_tests_app/test/gamestate_test.exs
+++ server/rust_tests_app/test/gamestate_test.exs
@@ -1,22 +1,27 @@
 defmodule GameStateTesting do
   use ExUnit.Case
 
+  @tag :skip
   test "No move if beyond boundaries" do
     assert {:ok, ""} = TestNIFs.no_move_if_beyond_boundaries()
   end
 
+  @tag :skip
   test "No move if occupied" do
     assert {:ok, ""} = TestNIFs.no_move_if_occupied()
   end
 
+  @tag :skip
   test "No move if wall" do
     assert {:ok, ""} = TestNIFs.no_move_if_wall()
   end
 
+  @tag :skip
   test "Attacking" do
     assert {:ok, ""} = TestNIFs.attacking()
   end
 
+  @tag :skip
   test "Movement" do
     assert {:ok, ""} = TestNIFs.movement()
   end
