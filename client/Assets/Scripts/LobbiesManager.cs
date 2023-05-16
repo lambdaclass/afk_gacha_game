5e4b14d15c34c362bdc9f47e562abd7b12dddf6f
BertovDev <bertobautista18@gmail.com>
2023-05-16 15:35:44 -0300

    refactor lobbyManager

---
+++ client/Assets/Scripts/LobbiesManager.cs
@@ -0,0 +1,41 @@
+using MoreMountains.TopDownEngine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbiesManager : LevelSelector
+{
+    [SerializeField]
+    Text sessionId;
+
+    public override void GoToLevel()
+    {
+        base.GoToLevel();
+    }
+
+    public void CreateLobby()
+    {
+        LobbyConnection.Instance.CreateLobby();
+        GoToLevel();
+    }
+
+    public void ConnectToLobby()
+    {
+        LobbyConnection.Instance.ConnectToLobby(sessionId.text);
+        GoToLevel();
+    }
+
+    public void Back()
+    {
+        LobbyConnection.Instance.Init();
+        GoToLevel();
+    }
+
+    // void update()
+    // {
+    //     if (!String.IsNullOrEmpty(LobbyConnection.Instance.GameSession))
+    //     {
+    //         GoToLevel();
+    //     }
+    // }
+}
--- client/Assets/Scripts/LobbyManager.cs
+++ client/Assets/Scripts/LobbyManager.cs
@@ -4,27 +4,38 @@ using System.Collections.Generic;
 using MoreMountains.TopDownEngine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LobbyManager : LevelSelector
 {
+    [SerializeField] GameObject playButton;
+
     public override void GoToLevel()
     {
         base.GoToLevel();
-        if (SceneManager.GetActiveScene().name == "Lobbies")
+    }
+
+    void Start()
+    {
+        if (LobbyConnection.Instance.playerId == 1)
         {
-            LobbyConnection.Instance.CreateLobby();
+            playButton.SetActive(true);
         }
         else
         {
-            LobbyConnection.Instance.StartGame();
+            playButton.SetActive(false);
         }
     }
 
-    void update()
+    public void GameStart()
     {
-        if (!String.IsNullOrEmpty(LobbyConnection.Instance.GameSession))
-        {
-            GoToLevel();
-        }
+        LobbyConnection.Instance.StartGame();
+        GoToLevel();
+    }
+
+    public void Back()
+    {
+        LobbyConnection.Instance.Init();
+        GoToLevel();
     }
 }
