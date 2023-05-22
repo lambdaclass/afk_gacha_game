48d18a6bb7bbf7b5edebe9d0a0c8c741fd84134e
Martina <martina@martina>
2023-05-22 17:46:10 -0300

    initial commit

--- server/native/gamestate/src/game.rs
+++ server/native/gamestate/src/game.rs
@@ -118,6 +118,40 @@ impl GameState {
         );
     }
 
+    pub fn move_player_to_arbitrary_position(self:&mut Self, player_id: u64, new_position:(u16, u16)) {
+        let player = self
+            .players
+            .iter_mut()
+            .find(|player| player.id == player_id)
+            .unwrap();
+
+        if matches!(player.status, Status::DEAD) {
+            return;
+        }
+
+        // These changes are done so that if the player is moving into one of the map's borders
+        // but is not already on the edge, they move to the edge. In simpler terms, if the player is
+        // trying to move from (0, 1) to the left, this ensures that new_position is (0, 0) instead of
+        // something invalid like (0, -1).
+        new_position.x = min(new_position.x, self.board.height - 1);
+        new_position.x = max(new_position.x, 0);
+        new_position.y = min(new_position.y, self.board.width - 1);
+        new_position.y = max(new_position.y, 0);
+
+        let tile_to_move_to = tile_to_move_to(&self.board, &player.position, &new_position);
+
+        // Remove the player from their previous position on the board
+        self.board
+            .set_cell(player.position.x, player.position.y, Tile::Empty);
+
+        player.position = tile_to_move_to;
+        self.board.set_cell(
+            player.position.x,
+            player.position.y,
+            Tile::Player(player.id),
+        );
+    }
+
     pub fn attack_player(self: &mut Self, attacking_player_id: u64, attack_direction: Direction) {
         let attacking_player = self
             .players
