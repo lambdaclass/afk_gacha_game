0833bd67e33de3d1698df3c9a12b86f0fd769969
tvillegas98 <tvillegas@fi.uba.ar>
2023-05-29 16:09:18 -0300

    Elixir: Pass the game config to the game

--- server/lib/dark_worlds_server_web/websockets/lobby_websocket.ex
+++ server/lib/dark_worlds_server_web/websockets/lobby_websocket.ex
@@ -27,8 +27,8 @@ defmodule DarkWorldsServerWeb.LobbyWebsocket do
 
   def websocket_handle({:binary, message}, state) do
     case Communication.lobby_decode(message) do
-      {:ok, %{type: :START_GAME}} ->
-        Matchmaking.start_game(state[:lobby_pid])
+      {:ok, %{type: :START_GAME} = message} ->
+        Matchmaking.start_game(message[:game_config], state[:lobby_pid])
         {:ok, state}
 
       {:error, msg} ->
