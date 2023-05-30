b4719b5d827086b3ec43114a84785632cc13242e
Francisco Krause Arnim <56402156+fkrause98@users.noreply.github.com>
2023-05-30 14:09:09 -0300

    Delete status.ex

--- server/lib/dark_worlds_server/engine/status.ex
+++
@@ -1,6 +0,0 @@
-defmodule DarkWorldsServer.Engine.Status do
-  use DarkWorldsServer.Communication.Encoder
-
-  @enforce_keys [:id, :health, :position, :last_melee_attack, :status]
-  defstruct [:id, :health, :position, :last_melee_attack, :status]
-end
