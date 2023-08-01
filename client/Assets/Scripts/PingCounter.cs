dd156d520167735bf9558678f0e5c806279746e4
Nicolas Continanza <nicolas.continanza@lambdaclass.com>
2023-08-01 16:09:28 -0300

    Improve metrics info display (#684)
    * WIP
    
    * Refactor metrics
    
    * Improve client and server hashes display
    
    * Restore changes in scene
    
    * Adjust positions
    
    * Some visual adjustments
    
    * Solve prefab conflict with the scene
    
    * Anchor metrics bar to the bottom of the screen
    
    * Adjust texts, positions and sizes

--- client/Assets/Scripts/PingCounter.cs
+++ client/Assets/Scripts/PingCounter.cs
@@ -8,35 +8,35 @@ using System.Collections;
 public class PingCounter : MonoBehaviour
 {
     /// the frequency at which the PING counter should update
-	public float UpdateInterval = 5f;
+    public float UpdateInterval = 5f;
     protected float _timeLeft;
     protected Text _text;
 
     /// <summary>
-	/// On Start(), we get the Text component and initialize our counter
-	/// </summary>
-	protected virtual void Start()
-	{
-		if(GetComponent<Text>()==null)
-		{
-			Debug.LogWarning ("PINGCounter requires a GUIText component.");
-			return;
-		}
-		_text = GetComponent<Text>();
+    /// On Start(), we get the Text component and initialize our counter
+    /// </summary>
+    protected virtual void Start()
+    {
+        if (GetComponent<Text>() == null)
+        {
+            Debug.LogWarning("PINGCounter requires a GUIText component.");
+            return;
+        }
+        _text = GetComponent<Text>();
         _timeLeft = UpdateInterval;
-	}
+    }
 
     /// <summary>
-	/// On Update, we decrease our time_left counter, and if we've reached zero, we update our PING counter
-	/// with the last PING received
-	/// </summary>
-	protected virtual void Update()
-	{
+    /// On Update, we decrease our time_left counter, and if we've reached zero, we update our PING counter
+    /// with the last PING received
+    /// </summary>
+    protected virtual void Update()
+    {
         _timeLeft = _timeLeft - Time.deltaTime;
-		if( _timeLeft <= 0.0 )
+        if (_timeLeft <= 0.0)
         {
             _timeLeft = UpdateInterval;
-            _text.text = "PING: " + SocketConnectionManager.Instance.currentPing.ToString() + " ms";
+            _text.text = "PING " + SocketConnectionManager.Instance.currentPing.ToString() + " ms";
         }
-	}
+    }
 }
--- client/Assets/Scripts/VersionsHashes.cs
+++ client/Assets/Scripts/VersionsHashes.cs
@@ -1,23 +1,21 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 
 public class VersionsHashes : MonoBehaviour
 {
-	public void Start()
-	{
-		if(GetComponent<Text>() == null)
-		{
-			Debug.LogWarning ("VersionsHashes requires a GUIText component.");
-			return;
-		}
+    public bool serverHash;
 
-        string server_hash = "Server hash \t" + SocketConnectionManager.Instance.serverHash;
-        string client_hash = "Client hash \t" + GitInfo.GetGitHash();
-		GetComponent<Text>().text = server_hash + "\n" + client_hash;
-	}
+    public void Start()
+    {
+        if (GetComponent<Text>() == null)
+        {
+            Debug.LogWarning("VersionsHashes requires a GUIText component.");
+            return;
+        }
 
-	public void Update()
-	{
-	}
+        string hash = serverHash
+            ? "SERVER #" + SocketConnectionManager.Instance.serverHash
+            : "CLIENT #" + GitInfo.GetGitHash();
+        GetComponent<Text>().text = hash;
+    }
 }
