80d3c78f746ef01cc4da22790e0b7a8f4e85fbab
An <82987608+AngieDutra@users.noreply.github.com>
2023-08-03 14:16:26 -0300

    Lobbies UI UX updates (#707)

--- client/Assets/Scripts/UIManager.cs
+++ client/Assets/Scripts/UIManager.cs
@@ -12,14 +12,23 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     GameObject lobbyItemPrefab;
 
+    [SerializeField]
+    GameObject noLobbiesText;
+
     bool lobbiesEmpty = true;
     bool gamesEmpty = true;
+
+    void Start()
+    {
+        noLobbiesText.SetActive(lobbiesEmpty);
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (lobbiesEmpty && LobbyConnection.Instance.lobbiesList.Count > 0)
         {
+            noLobbiesText.SetActive(false);
             GenerateList(LobbyConnection.Instance.lobbiesList, lobbyItemPrefab, lobbiesContainer);
             lobbiesEmpty = false;
         }
@@ -27,6 +36,7 @@ public class UIManager : MonoBehaviour
 
     public void GenerateList(List<string> itemList, Object itemPrefab, Transform container)
     {
+        itemList.Reverse();
         itemList.ForEach(el =>
         {
             GameObject item = (GameObject)Instantiate(itemPrefab, container);
