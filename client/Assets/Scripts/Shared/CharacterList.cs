652a1d79b268b344062a9118710b9a8209489fbd
Theo <theokatz@gmail.com>
2023-12-14 16:51:48 -0300

    populate list

--- client/Assets/Scripts/Shared/CharacterList.cs
+++ client/Assets/Scripts/Shared/CharacterList.cs
@@ -1,17 +1,37 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class CharacterList : MonoBehaviour
 {
     [SerializeField]
     GameObject CharacterItemUIPrefab;
 
+    [SerializeField]
+    GameObject CharacterListContainer;
+
+    [SerializeField]
+    List<Character> characters;
+
     [NonSerialized]
     public UnityEvent<Character> OnCharacterSelected;
 
-    void Start() {
+    void Start()
+    {
         OnCharacterSelected = new UnityEvent<Character>();
+        PopulateList();
+    }
+
+    private void PopulateList()
+    {
+        characters.ForEach(character =>
+        {
+            GameObject characterItem = Instantiate(CharacterItemUIPrefab, CharacterListContainer.transform);
+            characterItem.GetComponent<Image>().sprite = character.characterSprite;
+            characterItem.GetComponent<Button>().onClick.AddListener(() => SelectCharacter(character));
+        });
     }
 
     public void SelectCharacter(Character character)
