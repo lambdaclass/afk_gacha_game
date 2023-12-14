16a67c719b8edb5ece187adaad4c4ef4bbcb0f9e
Theo <theokatz@gmail.com>
2023-12-14 14:31:02 -0300

    add characterSO, muflus SO & prefab

---
+++ client/Assets/Scripts/Character.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Character", menuName = "Character")]
+public class Character : ScriptableObject
+{
+    public new string name;
+    public Sprite characterSprite;
+    public GameObject prefab;
+}
