9fedb8cb3c65d8bc03489e374a126cad45806680
Theo Katz <67983386+tkz00@users.noreply.github.com>
2023-12-18 14:42:24 -0300

    remove old scripts, scenes and other assets (#4)

--- client/Assets/Scripts/LootableList.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "CoM Lootable List", fileName = "Lootable List")]
-public class LootableList : ScriptableObject
-{
-    public List<Lootable> LootList;
-}
