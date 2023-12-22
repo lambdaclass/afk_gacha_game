b90e4376c534c976beb2cb6b4e244fdaf85d3288
Theo Katz <67983386+tkz00@users.noreply.github.com>
2023-12-22 16:13:55 -0300

    Create unit detail scene (#33)
    * remove old scripts, scenes and other assets
    
    * remove prefab's folder
    
    * add third party assets to gitignore
    
    * add overworld scene & first location
    
    * add map location prefab
    
    * modify location text color & add new locations to overworld
    
    * create header prefab & add it to overworld scene
    
    * add level manager & campaign scene
    
    * make header responsive
    
    * update overworld location marker size
    
    * update overworld scene locations positions
    
    * add scenes to build settings
    
    * set up campaign markers and battle button
    
    * add lineup scene
    
    * lineup scene character list
    
    * add lineup scene
    
    * lineup scene character list
    
    * add lineup scene to build settings
    
    * reorganize scripts, prefabs & add character selection logic
    
    * add characterSO, muflus SO & prefab
    
    * spawn model on character select
    
    * populate list
    
    * populate character list based on assigned scriptable objects
    
    * five unit positions in scene
    
    * add uma scriptableObject
    
    * add uma to lineup and fix scroll bug
    
    * fix lineup units positions
    
    * create battle scene
    
    * add controls to battle scene
    
    * add victory splash
    
    * add defeat splash
    
    * deactivate endgame splashes
    
    * add camera prefab
    
    * rename camera prefab
    
    * use camera prefab on scenes
    
    * create unit list scene
    
    * change character to unit & refactor unit list item button setup
    
    * select unit & load unit detail scene
    
    * fix unit list & unit detail backgrounds and add prev & next unit buttons in detail
    
    * add character specific background to unit detail scene & add level up costs
    
    * add item slots, modify stats size & change next & prev arrow color
    
    * footer sizes & positions
    
    * a lot of things of showing models in lineup scene
    
    * now works with one lineup space
    
    * select and display correct 3d models in lineup
    
    * fix background aspect ratio
    
    * fix uma scriptable object
    
    * select unit in unit list scene doesn't take to unit detail
    
    * fix unit detail background aspect ratio & refactor unit list transition to unit detail
    
    * merge corrections

--- client/Assets/Scripts/UnitDetail/UnitDetail.cs
+++ client/Assets/Scripts/UnitDetail/UnitDetail.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UnitDetail : MonoBehaviour
 {
     private static Unit selectedUnit;
+
+    [SerializeField]
+    TMP_Text unitName;
+
+    [SerializeField]
+    Image background;
+
+    void Start() {
+        unitName.text = selectedUnit.name;
+        background.sprite = selectedUnit.backgroundSprite;
+    }
 
     public static void SelectUnit(Unit unit) {
         selectedUnit = unit;
