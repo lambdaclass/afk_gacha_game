a8ada806e2fc98f2b019636c3e3e6c9aa20cd95c
NContinanza <nicolas.continanza@lambdaclass.com>
2024-01-04 16:13:43 -0300

    Add selected units to battle screen

--- client/Assets/Scripts/Battle/BattleManager.cs
+++ client/Assets/Scripts/Battle/BattleManager.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleManager : MonoBehaviour
 {
@@ -14,12 +18,54 @@ public class BattleManager : MonoBehaviour
     [SerializeField]
     UnitPosition[] opponentUnitPositions;
 
+    [SerializeField]
+    List<Character> characters;
 
     string playerDeviceId = "user1";
     string opponentId;
 
     void Start()
     {
+        StartCoroutine(
+            BackendConnection.GetAvailableUnits(
+                "user1",
+                units => {
+                    List<Unit> unitList = units.Select(unit => new Unit
+                    {
+                        unitId = unit.id,
+                        level = unit.level,
+                        character = characters.Find(character => unit.character.ToLower() == character.name.ToLower()),
+                        slot = unit.slot,
+                        selected = unit.selected
+                    }).ToList();
+                    SetUpSelectedUnits(unitList, true);
+                },
+                error => {
+                    Debug.LogError("Error when getting the available units: " + error);
+                }
+            )
+        );
+
+        StartCoroutine(
+            BackendConnection.GetAvailableUnits(
+                "user2",
+                units => {
+                    List<Unit> unitList = units.Select(unit => new Unit
+                    {
+                        unitId = unit.id,
+                        level = unit.level,
+                        character = characters.Find(character => unit.character.ToLower() == character.name.ToLower()),
+                        slot = unit.slot,
+                        selected = unit.selected
+                    }).ToList();
+                    SetUpSelectedUnits(unitList, false);
+                },
+                error => {
+                    Debug.LogError("Error when getting the available units: " + error);
+                }
+
+            )
+        );
         StartCoroutine(
             BackendConnection.GetOpponents
             (
@@ -45,5 +91,15 @@ public class BattleManager : MonoBehaviour
                 }
             )
         );
+    }
+
+    private void SetUpSelectedUnits(List<Unit> units, bool isPlayer)
+    {
+        UnitPosition[] unitPositions = isPlayer ? playerUnitPositions : opponentUnitPositions;
+        foreach(Unit unit in units.Where(unit => unit.selected && unit.slot.Value < unitPositions.Length)) {
+            UnitPosition unitPosition;
+            unitPosition = unitPositions[unit.slot.Value];
+            unitPosition.SetUnit(unit, isPlayer);
+        }
     }
 }
