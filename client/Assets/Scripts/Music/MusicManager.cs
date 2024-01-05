5bef520e65b55dc5d47379695bbc8d3af8f6a2ba
NContinanza <nicolas.continanza@lambdaclass.com>
2024-01-05 10:34:58 -0300

    WIP: mute button

--- client/Assets/Scripts/Music/MusicManager.cs
+++ client/Assets/Scripts/Music/MusicManager.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicManager : MonoBehaviour
 {
     private AudioSource _audioSource;
+    public Button audioToggleButton;
+    public Image audioToggle;
+    public Sprite audioOn;
+    public Sprite audioOff;
     public static MusicManager Instance { get; private set; }
     
     private void Awake()
@@ -11,11 +16,25 @@ public class MusicManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(transform.gameObject);
+            DontDestroyOnLoad(audioToggleButton.gameObject);
             _audioSource = GetComponent<AudioSource>();
+            
         }
         else
         {
             Destroy(gameObject);
         }
+    }
+
+    public void ToggleMusic()
+    {
+        if (_audioSource.isPlaying) 
+        {
+            _audioSource.Pause();
+            audioToggle.sprite = audioOff;
+            return;
+        }
+        _audioSource.UnPause();
+        audioToggle.sprite = audioOn;
     }
 }
