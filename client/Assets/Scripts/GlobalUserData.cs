c18e5516182290fba20930658b0075ecb9ed33c2
lotuuu <nicolas@lotufo.com.ar>
2024-01-05 17:52:07 -0300

    Fix positions

--- client/Assets/Scripts/GlobalUserData.cs
+++ client/Assets/Scripts/GlobalUserData.cs
@@ -48,11 +48,11 @@ public class GlobalUserData : MonoBehaviour
                 username = "SampleUser",
                 units = new List<Unit>
                 {
-                    new Unit { id = "101", level = 5, character = characters.Find(character => "muflus" == character.name.ToLower()), slot = 1, selected = true },
-                    new Unit { id = "102", level = 5, character = characters.Find(character => "muflus" == character.name.ToLower()), slot = 2, selected = true },
-                    new Unit { id = "103", level = 5, character = characters.Find(character => "muflus" == character.name.ToLower()), slot = 3, selected = true },
-                    new Unit { id = "104", level = 5, character = characters.Find(character => "muflus" == character.name.ToLower()), slot = 4, selected = true },
-                    new Unit { id = "105", level = 5, character = characters.Find(character => "muflus" == character.name.ToLower()), slot = 5, selected = true }
+                    new Unit { id = "101", level = 5, character = characters.Find(character => "muflus" == character.name.ToLower()), slot = 0, selected = true },
+                    new Unit { id = "102", level = 5, character = characters.Find(character => "muflus" == character.name.ToLower()), slot = 1, selected = true },
+                    new Unit { id = "103", level = 5, character = characters.Find(character => "muflus" == character.name.ToLower()), slot = 2, selected = true },
+                    new Unit { id = "104", level = 5, character = characters.Find(character => "muflus" == character.name.ToLower()), slot = 3, selected = true },
+                    new Unit { id = "105", level = 5, character = characters.Find(character => "muflus" == character.name.ToLower()), slot = 4, selected = true }
                 }
             };
         } else {
