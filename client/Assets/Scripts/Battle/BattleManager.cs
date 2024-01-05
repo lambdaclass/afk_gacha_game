16ff5dd5660ae7332d078072fcb5cdec346a66b5
Nicolas Continanza <nicolas.continanza@lambdaclass.com>
2024-01-05 11:41:08 -0300

    Add selected units to Battle scene (#56)
    * add dragons asset
    
    * add golem asset
    
    * golem scriptable object
    
    * golem UI model & animator
    
    * fix camera rotation in lineup scene & reorganize character assets
    
    * rename character UI prefabs
    
    * fix unit model rotation
    
    * fix uma's UI model scale
    
    * add zombie to lineup
    
    * RedUsurperDragon UI model & animator
    
    * add opponent model display textures
    
    * fix opponent positions & scale RedDragon
    
    * change unit name color
    
    * change character's prefabs folder
    
    * connect with the backend
    
    * Remove PlayerLineupManager script, add LineupManager to populate unit list
    
    * get units with positions
    
    * select unit in lineup
    
    * select & deselect units in lineup (deselect doesn't fully work)
    
    * Battle & lineup work, unitList doesn't
    
    * unitList also works
    
    * everything works except selecting unit after unselecting
    
    * everything works
    
    * take IUnitPopulator to own file
    
    * Remove ThirdParty Assets
    
    * Add ThirdParty and TextMesh Pro to gitignore
    
    * Add ThirdParty and TextMesh Pro to gitignore
    
    * Move third party animations to an ignored folder
    
    * Remove meta file
    
    * Complete backend connection on the battle simulation. Update readme
    
    * Remove duplicate code
    
    * Add error callbacks
    
    * Remove duplicate code and minor refactor
    
    * Clean up code
    
    * Remove comments associated to https://github.com/lambdaclass/champions_of_mirra/issues/50
    
    * WIP: Adapt to recent backend changes
    
    * Fix endpoint
    
    * Simplify units logic
    
    * wip
    
    * Add selected units to battle screen
    
    * Remove print
    
    * Remove duplicate code
    
    * Simplify BattleManager implementation
    
    * Rotate opponent units
    
    ---------
    
    Co-authored-by: Theo <theokatz@gmail.com>

--- client/Assets/Scripts/Battle/BattleManager.cs
+++ client/Assets/Scripts/Battle/BattleManager.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleManager : MonoBehaviour
 {
@@ -14,34 +18,77 @@ public class BattleManager : MonoBehaviour
     [SerializeField]
     UnitPosition[] opponentUnitPositions;
 
+    [SerializeField]
+    List<Character> characters;
 
-    string playerDeviceId = "user1";
-    string opponentId;
+    readonly string playerDeviceId = "user1";
 
     void Start()
     {
+        GetAndSetUpUserAvailableUnits(playerDeviceId, true);
+
+        GetAndSetUpUserAvailableUnits("user2", false);
+
         StartCoroutine(
             BackendConnection.GetOpponents
             (
                 playerDeviceId, opponents => {
-                    this.opponentId = opponents[0].id;
-                    StartCoroutine(
-                        BackendConnection.GetBattleResult(playerDeviceId, this.opponentId,
-                        winnerId => {
-                            if(winnerId == opponentId) {
-                                defeatSplash.SetActive(true);
-                            } else {
-                                victorySplash.SetActive(true);
-                            }
-                        },
-                        error => {
-                            Debug.LogError("Error when getting the battle result: " + error);
-                        }
-                        )
-                    );
+                    string opponentId = opponents[0].id;
+                    SimulateBattle(playerDeviceId, opponentId);                    
                 },
                 error => {
                     Debug.LogError("Error when getting the opponents: " + error);
+                }
+            )
+        );
+    }
+
+    private void SetUpSelectedUnits(List<Unit> units, bool isPlayer)
+    {
+        UnitPosition[] unitPositions = isPlayer ? playerUnitPositions : opponentUnitPositions;
+        foreach(Unit unit in units.Where(unit => unit.selected && unit.slot.Value < unitPositions.Length)) {
+            UnitPosition unitPosition;
+            unitPosition = unitPositions[unit.slot.Value];
+            unitPosition.SetUnit(unit, isPlayer);
+        }
+    }
+
+    private void GetAndSetUpUserAvailableUnits(string playerDeviceId, bool isPlayer)
+    {
+        StartCoroutine(
+            BackendConnection.GetAvailableUnits(
+                playerDeviceId,
+                units => {
+                    List<Unit> unitList = units.Select(unit => new Unit
+                    {
+                        unitId = unit.id,
+                        level = unit.level,
+                        character = characters.Find(character => unit.character.ToLower() == character.name.ToLower()),
+                        slot = unit.slot,
+                        selected = unit.selected
+                    }).ToList();
+                    SetUpSelectedUnits(unitList, isPlayer);
+                },
+                error => {
+                    Debug.LogError("Error when getting the available units: " + error);
+                }
+            )
+        );
+    }
+
+    private void SimulateBattle(string playerDeviceId, string opponentId)
+    {
+        StartCoroutine(
+            BackendConnection.GetBattleResult(playerDeviceId, opponentId,
+                winnerId => {
+                    if(winnerId == opponentId) {
+                        defeatSplash.SetActive(true);
+                    } else {
+                        victorySplash.SetActive(true);
+                    }
+                },
+                error => {
+                    Debug.LogError("Error when getting the battle result: " + error);
                 }
             )
         );
