d6607e7648a7ad1f1575fccc3f5141af826b6a38
NContinanza <nicolas.continanza@lambdaclass.com>
2024-01-08 16:47:06 -0300

    WIP: campaign scene and campaign manager

---
+++ client/Assets/Scripts/Campaign/Campaign.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Campaign : MonoBehaviour
+{
+    public void Start() {
+        
+    }
+}
---
+++ client/Assets/Scripts/Campaign/CampaignManager.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CampaignManager : MonoBehaviour
+{
+    [SerializeField]
+    List<Campaign> campaigns;
+
+    public void Start() {
+        
+    }
+}
