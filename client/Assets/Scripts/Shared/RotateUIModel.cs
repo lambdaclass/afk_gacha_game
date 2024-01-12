bd32289f6ef936e2befaf4a0cc15c0ba88e38594
Nicolas Continanza <nicolas.continanza@lambdaclass.com>
2024-01-12 16:15:11 -0300

    Summon a hero (#77)
    * add summon scene without functionality, edit minor detail about header, add functionality to the Back button in Lineup
    
    * Conflicts fixed
    
    * Basic gacha summon
    
    * Aesthetics and visuals improvements
    
    * WIP
    
    * Visual adjustments on scene
    
    * Character position and light
    
    * Lighting improvement
    
    * WIP: button animation
    
    * Adjust positions in scene
    
    * Button Animation to support character rotation, and going to the unit details scene when it's clicked
    
    * Fix rotation bug when a second summoned character was rotated
    
    * Remove unused event
    
    * Remove prints
    
    * Remove unused methods
    
    * Tidy up the usage of AcceptBehaviour in the Summon scene
    
    * Clean up code
    
    * Simplify SelectUnit method
    
    * Clean imports
    
    * More detailed DOTween explanation
    
    ---------
    
    Co-authored-by: Martina Cantaro <martina@lambdaclass.com>

---
+++ client/Assets/Scripts/Shared/RotateUIModel.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class RotateUIModel : MonoBehaviour, IDragHandler
+{
+    [SerializeField]
+    float rotationSpeed = 1f;
+
+    [SerializeField]
+    GameObject modelContainer;
+    Transform model;
+    Touch touch;
+    Quaternion rotationX;
+    Coroutine coroutine;
+
+    void Start()
+    {
+        StartCoroutine(GetModel());
+    }
+
+    public IEnumerator GetModel()
+    {
+        yield return new WaitUntil(() => modelContainer.transform.childCount > 0);
+        model = modelContainer.transform.GetChild(0).transform;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        touch = Input.GetTouch(0);
+        StartCoroutine(RotateModel(touch));
+    }
+
+    IEnumerator RotateModel(Touch touch)
+    {
+        yield return GetModel();
+        
+        if (touch.phase == TouchPhase.Moved)
+        {
+            rotationX = Quaternion.Euler(0f, -touch.deltaPosition.x * rotationSpeed, 0f);
+            model.rotation *= rotationX;
+        }
+    }
+}
