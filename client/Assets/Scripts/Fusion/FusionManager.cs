1517a65c2029c541cfa4001d9bb397952f028e21
Nicolas Continanza <nicolas.continanza@lambdaclass.com>
2024-01-17 12:52:53 -0300

    Hero fusion (#104)
    * WIP: new screen
    
    * Basic character display functionality
    
    * Minor improvements and unselect method
    
    * Revert unwanted changes
    
    * Champion Fusion (#95)
    
    * Remove leftover print
    
    * Revert "Remove leftover print"
    
    This reverts commit dd64f16973c266f9a1f610f9c81cff0312efdba0.
    
    * Add champion level-up
    
    * Add Tiers and Rarity & Level/Tier-Up
    
    * Add CanFuseUnits validation
    
    * Add unit fusion
    
    * Change null for empty list
    
    * Replace hardcoded strings by new Currency enum values
    
    ---------
    
    Co-authored-by: NContinanza <nicolas.continanza@lambdaclass.com>
    
    * Hero fusion UI improvements (#106)
    
    * Adjust buttons positions in screen
    
    * Repopulate after changes in User units list
    
    * Add selected unit sprite and improve naming
    
    * Highlight selected unit in fusion screen
    
    * Fusion popup
    
    * Remove unused class
    
    * Remove comment
    
    * Add units to facilitate the fusion testing
    
    * Clear selected units list after failed fusion
    
    * Remove debugging logs
    
    * Select and unselect unit icons
    
    * Remove log
    
    * UnitsUIContainer Hierarchy to remove duplicate code
    
    * Smaller character in the fusion scene
    
    * Add selected unit name and rank. Logic for selection/unselection
    
    * Rename Quality to Rank
    
    * Fix unit list
    
    ---------
    
    Co-authored-by: Lotu <nicolas@lotufo.com.ar>

---
+++ client/Assets/Scripts/Fusion/FusionManager.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FusionManager : MonoBehaviour, IUnitPopulator
+{
+    [SerializeField] GameObject modelContainer;
+    [SerializeField] Button fusionButton;
+    [SerializeField] FusionUnitsUIContainer unitsContainer;
+    [SerializeField] GameObject characterNameContainer;
+
+    private List<Unit> selectedUnits;
+
+    GlobalUserData globalUserData = GlobalUserData.Instance;
+
+    void Start()
+    {
+        List<Unit> units = globalUserData.Units;
+        selectedUnits = new List<Unit>();
+        this.unitsContainer.Populate(units, this);
+    }
+
+    public void Populate(Unit unit, GameObject unitUIItem)
+    {
+        unitUIItem.GetComponent<Image>().sprite = unit.character.availableSprite;
+        Button unitItemButton = unitUIItem.GetComponent<Button>();
+        unitItemButton.onClick.AddListener(() => {
+            if (selectedUnits.Contains(unit))
+            {
+                UnselectUnit(unit);
+            }
+            else
+            {
+                SelectUnit(unit);
+            }
+        });
+    }
+
+    public void SelectUnit(Unit unit)
+    {
+        DisplayUnit(unit);
+        selectedUnits.Add(unit);
+        fusionButton.gameObject.SetActive(true);
+    }
+
+    public void UnselectUnit(Unit unit)
+    {
+        selectedUnits.Remove(unit);
+        if (selectedUnits.Count > 0)
+        {
+            DisplayUnit(selectedUnits[selectedUnits.Count - 1]);
+        }
+        else
+        {
+            RemoveUnitFromContainer(unit);
+            fusionButton.gameObject.SetActive(false);
+        }
+    }
+
+    private void DisplayUnit(Unit unit)
+    {
+        if (modelContainer.transform.childCount > 0)
+        {
+            RemoveUnitFromContainer(unit);
+        }
+        Instantiate(unit.character.prefab, modelContainer.transform);
+        characterNameContainer.GetComponentInChildren<TextMeshProUGUI>().text = unit.character.name + "\n Rank: " + unit.rank.ToString();
+        characterNameContainer.SetActive(true);
+    }
+
+    private void RemoveUnitFromContainer(Unit unit)
+    {
+        Destroy(modelContainer.transform.GetChild(0).gameObject);
+        characterNameContainer.SetActive(false);
+    }
+
+    public void Fusion() {
+        globalUserData.User.FuseUnits(selectedUnits);
+        this.unitsContainer.Populate(globalUserData.Units, this);
+        selectedUnits.Clear();
+        fusionButton.gameObject.SetActive(false);
+    }
+}
---
+++ client/Assets/Scripts/Lineup/LineUpUnitsUIContainer.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LineUpUnitsUIContainer : UnitsUIContainer
+{
+    public override void Populate(List<Unit> units, IUnitPopulator unitPopulator = null)
+    {
+        unitsContainer.SetActive(false);
+        this.Clear();
+        units.ForEach(unit =>
+        {
+            GameObject unitUIItem = Instantiate(unitItemUIPrefab, unitsContainer.transform);
+            unitUIItem.GetComponent<Image>().sprite = unit.character.availableSprite;
+            Button unitItemButton = unitUIItem.GetComponent<Button>();
+            unitItemButton.onClick.AddListener(() => SelectUnit(unit, unitItemButton.gameObject));
+            if (unitPopulator != null)
+            {
+                unitPopulator.Populate(unit, unitUIItem);
+            }
+            unitUIItemDictionary.Add(unit.id, unitUIItem);
+        });
+        unitsContainer.SetActive(true);
+    }
+
+    public override void SelectUnit(Unit unit, GameObject selector)
+    {
+        Button unitItemButton = selector.GetComponent<Button>();
+        OnUnitSelected.Invoke(unit);
+        unitItemButton.interactable = false;
+    }
+
+    public void SetUnitUIActiveById(string unitId)
+    {
+        unitUIItemDictionary[unitId].GetComponent<Button>().interactable = true;
+    }
+}
