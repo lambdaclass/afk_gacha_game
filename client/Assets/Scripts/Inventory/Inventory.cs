38e9a1fb42774bc79c6b81b3afebb3d66d5d88d4
Theo Katz <67983386+tkz00@users.noreply.github.com>
2024-02-20 15:20:44 -0300

    Inventory screen (#171)
    * add inventory scene & spawn item icons on grid
    
    * add item detail popup

---
+++ client/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField]
+    GameObject itemPrefab;
+    [SerializeField]
+    GameObject itemsContainer;
+    [SerializeField]
+    ItemDetailPopup itemDetailPopup;
+
+    void Start()
+    {
+        foreach(var group in GlobalUserData.Instance.User.items.GroupBy(item => item.template.id)) {
+            GameObject itemUIObject = Instantiate(itemPrefab, itemsContainer.transform);
+            itemUIObject.GetComponent<InventoryItemUI>().SetUpItem(group.First(), group.Count());
+            Button unitItemButton = itemUIObject.GetComponent<Button>();
+            unitItemButton.onClick.AddListener(() => ShowItemDetailPopup(group.First()));
+        }
+    }
+
+    private void ShowItemDetailPopup(Item item)
+    {
+        itemDetailPopup.ShowItem(item);
+    }
+}
