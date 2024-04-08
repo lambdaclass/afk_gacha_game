7abb6960499f715b83c6e4ca79fcb9f8b1e38548
Theo Katz <67983386+tkz00@users.noreply.github.com>
2024-04-08 14:10:10 -0300

    Go next after beating level (#296)
    * add new currencies
    
    * clean up level progress
    
    * remove unused scripts
    
    * next level reference in battle manager
    
    * LevelProgress get's next level
    
    * next button takes the player to next level
    
    * remove unused methods
    
    * change texts
    
    * victory/defeat splash screen fully opaque
    
    * tap to return takes you to campaign instead of campaign map, also campaign indicators are updated live
    
    * don't set a next level when selected level is last one of campaign
    
    * next button on last level of campaign takes player back to campaigns map
    
    * next campaign takes to campaigns map
    
    * return text on victory and defeat splash screens higher
    
    * change 'tap to return' height
    
    * fix character list size in lineup
    
    * change loop index for clarity

--- client/Assets/Scripts/Battle/BattleManager.cs
+++ client/Assets/Scripts/Battle/BattleManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -18,14 +19,15 @@ public class BattleManager : MonoBehaviour
     [SerializeField]
     UnitPosition[] opponentUnitPositions;
 
-    public static LevelData selectedLevelData;
+	[SerializeField]
+	LevelManager levelManager;
 
     void Start()
     {
 		victorySplash.SetActive(false);
 		defeatSplash.SetActive(false);
         List<Unit> userUnits = GlobalUserData.Instance.Units;
-        List<Unit> opponentUnits = selectedLevelData.units;
+        List<Unit> opponentUnits = LevelProgress.selectedLevelData.units;
 
         SetUpUnits(userUnits, opponentUnits);
         Battle();
@@ -33,7 +35,7 @@ public class BattleManager : MonoBehaviour
 
     public void Battle()
     {
-        SocketConnection.Instance.Battle(GlobalUserData.Instance.User.id, selectedLevelData.id, (result) => {
+        SocketConnection.Instance.Battle(GlobalUserData.Instance.User.id, LevelProgress.selectedLevelData.id, (result) => {
             HandleBattleResult(result);
         });
     }
@@ -48,35 +50,47 @@ public class BattleManager : MonoBehaviour
 				Debug.LogError(ex.Message);
 			}
             GlobalUserData user = GlobalUserData.Instance;
-            user.AddCurrency(selectedLevelData.rewards);
-            user.AddExperience(selectedLevelData.experienceReward);
+            user.AddCurrency(LevelProgress.selectedLevelData.rewards);
+            user.AddExperience(LevelProgress.selectedLevelData.experienceReward);
             user.AccumulateAFKRewards();
-            user.User.afkMaxCurrencyReward = selectedLevelData.afkCurrencyRate;
-            user.User.afkMaxExperienceReward = selectedLevelData.afkExperienceRate;
+            user.User.afkMaxCurrencyReward = LevelProgress.selectedLevelData.afkCurrencyRate;
+            user.User.afkMaxExperienceReward = LevelProgress.selectedLevelData.afkExperienceRate;
             victorySplash.GetComponentInChildren<RewardsUIContainer>().Populate(CreateRewardsList());
             victorySplash.SetActive(true);
             victorySplash.GetComponent<AudioSource>().Play();
 
-            // Always shows the same texts when a win is achieved, but they should change based on if it was the last level of the campaign and if there are other campaigns after that
+            GameObject victoryText = victorySplash.transform.Find("CenterContainer").transform.Find("Sign").transform.Find("Text").gameObject;
 
-            // if (selectedLevelData.campaignToComplete == "") {
-            //     nextButton.GetComponentInChildren<Text>().text = "NEXT STAGE";
-            //     victoryText.GetComponent<Text>().text = "Victory!";
-            // } else {
-            //     // We assume this level's campaign is the one that was completed
-            //     victoryText.GetComponent<Text>().text = "Campaign beaten!";
-            //     if (selectedLevelData.campaignToUnlock == "") {
-            //         // No "next campaign". Just go back to CampaignsMap.
-            //         nextButton.SetActive(false);
-            //     } else {
-            //         nextButton.GetComponentInChildren<Text>().text = "BACK TO CAMPAIGNS";
-            //     }
-            // }
+            // Always shows the same texts when a win is achieved, but they should change based on if it was the last level of the campaign and if there are other campaigns after that
+            victoryText.GetComponent<Text>().text = "Victory!";
+
+			SetUpNextButton();
+			
+			// This should be handled differently
+			CampaignManager.selectedCampaignData.levels.Find(level => level.id == LevelProgress.selectedLevelData.id).status = LevelProgress.Status.Completed;
+			if(CampaignManager.selectedCampaignData.levels.Any(level => level.id == LevelProgress.nextLevelData.id)) {
+				CampaignManager.selectedCampaignData.levels.Find(level => level.id == LevelProgress.nextLevelData.id).status = LevelProgress.Status.Unlocked;
+			}
         } else {
             defeatSplash.SetActive(true);
             defeatSplash.GetComponent<AudioSource>().Play();
         }
     }
+
+	private void SetUpNextButton()
+	{
+		GameObject nextButton = victorySplash.transform.Find("Next").gameObject;
+		if(LevelProgress.selectedLevelData.campaignId != LevelProgress.nextLevelData.campaignId) {
+			nextButton.GetComponentInChildren<TMP_Text>().text = "NEXT CAMPAIGN";
+		} else {
+			nextButton.GetComponentInChildren<TMP_Text>().text = "NEXT LEVEL";
+			nextButton.GetComponent<Button>().onClick.AddListener(() => {
+				LevelProgress.selectedLevelData = LevelProgress.nextLevelData;
+				LevelProgress.nextLevelData = LevelProgress.NextLevel(LevelProgress.nextLevelData);
+				gameObject.GetComponent<LevelManager>().ChangeToScene("Lineup");
+			});
+		}
+	}
 
     private void SetUpUnits(List<Unit> userUnits, List<Unit> opponentUnits)
     {
@@ -97,24 +111,12 @@ public class BattleManager : MonoBehaviour
     private List<UIReward> CreateRewardsList() {
         List<UIReward> rewards = new List<UIReward>();
 
-        if (selectedLevelData.experienceReward != 0) { rewards.Add(new ExperienceUIReward(selectedLevelData.experienceReward)); }
+        if (LevelProgress.selectedLevelData.experienceReward != 0) { rewards.Add(new ExperienceUIReward(LevelProgress.selectedLevelData.experienceReward)); }
 
-        foreach (var currencyReward in selectedLevelData.rewards) {
+        foreach (var currencyReward in LevelProgress.selectedLevelData.rewards) {
             rewards.Add(new CurrencyUIReward(currencyReward.Key, currencyReward.Value));
         }
 
         return rewards;
-    }
-
-    public void Next() {
-        gameObject.GetComponent<LevelManager>().ChangeToScene("CampaignsMap");
-
-        // If nextLevel is null this won't do anything
-        // if (selectedLevelData.campaignToUnlock != "") {
-        //     gameObject.GetComponent<SceneManager>().ChangeToScene("CampaignsMap");
-        // } else {
-        //     CampaignManager.automaticLoadLevelName = selectedLevelData.nextLevel.name;
-        //     gameObject.GetComponent<SceneManager>().ChangeToScene("Campaign");
-        // }
     }
 }
---
+++ client/Assets/Scripts/Campaign/LevelProgress.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public static class LevelProgress
+{
+    public enum Status
+    {
+        Locked,
+        Unlocked,
+        Completed
+	}
+
+	public static List<Campaign> campaigns;
+
+	public static LevelData selectedLevelData;
+	public static LevelData nextLevelData;
+
+	public static LevelData NextLevel(LevelData level)
+	{
+		Campaign currentCampaign = campaigns.Find(campaign => campaign.campaignId == level.campaignId);
+		int levelIndex = currentCampaign.levels.FindIndex(lvl => lvl.levelNumber == level.levelNumber + 1);
+		if(levelIndex != -1) {
+			return currentCampaign.levels[levelIndex];
+		}
+		if(campaigns.Any(campaign => campaign.campaignNumber == currentCampaign.campaignNumber + 1)) {
+			return campaigns.Find(campaign => campaign.campaignNumber == currentCampaign.campaignNumber + 1).levels.Find(lvl => lvl.levelNumber == 1);
+		}
+		
+		Debug.Log("There are no more campaigns after this one");
+		return null;
+	}
+}
