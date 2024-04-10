8fd2e36523c772ca25957a1416cb487d93f62834
Martina Cantaro <6120897+martinacantaro@users.noreply.github.com>
2024-04-10 14:28:30 -0300

    Revert "Refactor campaign progression (#314)"
    This reverts commit bf1dd5c056cb4ecf55dfe24dcfb85b965c527748.

--- client/Assets/Scripts/User.cs
+++ client/Assets/Scripts/User.cs
@@ -35,5 +35,5 @@ public class User
     public Dictionary<Currency, int> afkMaxCurrencyReward = new Dictionary<Currency, int>();
     public int afkMaxExperienceReward = 0;
 
-	public List<(string superCampaignId, string campaignId, string levelId)> campaignsProgresses = new List<(string, string, string)>();
+	public List<(string campaignId, string levelId)> campaignsProgress = new List<(string, string)>();
 }
