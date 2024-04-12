d36642ae20de550b4dbda6cc68861cfcadf919ca
Theo <theokatz@gmail.com>
2024-04-12 11:05:11 -0300

    destroy battle indicator after tween completed

--- client/Assets/Scripts/Battle/BattleIndicator.cs
+++ client/Assets/Scripts/Battle/BattleIndicator.cs
@@ -14,10 +14,14 @@ public class BattleIndicator : MonoBehaviour
 		transform.localScale = new Vector3(0, 0, 0);
 		transform.DOScale(new Vector3(1, 1, 1), .5f).SetEase(Ease.InOutBack);
 		transform.DOMoveY(transform.position.y + .4f, .5f).SetEase(Ease.InSine).SetDelay(.5f);
-		indicatorText.DOFade(0f, .5f).SetEase(Ease.InCirc).SetDelay(.5f);
+		indicatorText.DOFade(0f, .5f).SetEase(Ease.InCirc).SetDelay(.5f).onComplete = DestroyGameObject;
     }
 
 	public void SetText(string text) {
 		indicatorText.text = text;
+	}
+
+	private void DestroyGameObject() {
+		Destroy(gameObject);
 	}
 }
