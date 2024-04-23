fad3a04b8302abcd712f6747051e3b97d7e038af
NContinanza <nicolas.continanza@lambdaclass.com>
2024-04-23 12:48:19 -0300

    Add Kaline Tree level to User

---
+++ client/Assets/Scripts/User/KalineTreeLevel.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+public class KalineTreeLevel {
+    public string id;
+    public int level;
+    public int fertilizer_cost;
+    public int gold_cost;
+    public List<string> unlock_features;
+}
---
+++ client/Assets/Scripts/User/User.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class User
+{
+    public string id { get; set; }
+
+    public string username { get; set; }
+
+    public List<Unit> units { get; set; }
+
+    public List<Item> items { get; set; }
+
+    public int next_unit_id;
+
+    public Dictionary<Currency, int> currencies;
+
+    public KalineTreeLevel kalineTreeLevel;
+
+    public int level = 1;
+
+    public int experience = 0;
+
+    public int experienceToNextLevel = 100;
+
+    public DateTime lastAfkRewardClaim = DateTime.Now;
+    public DateTime lastAfkRewardAccum = DateTime.Now;
+    public List<AfkRewardRate> afkRewardRates = new List<AfkRewardRate>();
+
+    public Dictionary<Currency, int> accumulatedCurrencyReward = new Dictionary<Currency, int>();
+
+    public Dictionary<Currency, int> afkMaxCurrencyReward = new Dictionary<Currency, int>();
+    public int afkMaxExperienceReward = 0;
+
+	public List<(string superCampaignId, string campaignId, string levelId)> campaignsProgresses = new List<(string, string, string)>();
+}
