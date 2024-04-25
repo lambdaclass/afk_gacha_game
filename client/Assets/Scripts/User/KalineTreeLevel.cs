42c1d1f3f14c2b744a224bd2fd9e787cfa685d30
NContinanza <nicolas.continanza@lambdaclass.com>
2024-04-25 18:24:19 -0300

    Remove unnecessary id field from KalineTreeLevel

--- client/Assets/Scripts/User/KalineTreeLevel.cs
+++ client/Assets/Scripts/User/KalineTreeLevel.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 
 public class KalineTreeLevel {
-    public string id;
     public int level;
     public int fertilizerLevelUpCost;
     public int goldLevelUpCost;
