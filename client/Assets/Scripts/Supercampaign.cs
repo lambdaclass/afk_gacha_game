6417214bbaf3775b4e94907e871f6081d0582add
NContinanza <nicolas.continanza@lambdaclass.com>
2024-05-09 17:47:44 -0300

    Supercampaign basic scriptable objects

---
+++ client/Assets/Scripts/Supercampaign.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Supercampaign", menuName = "Supercampaigns")]
+public class Supercampaign : ScriptableObject
+{
+    public new string name;
+    public Sprite background;
+}
