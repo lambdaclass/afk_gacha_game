6f3c4eae509161e71332076ee90b409c814da8ee
NContinanza <nicolas.continanza@lambdaclass.com>
2024-05-16 15:12:56 -0300

    Use addressables to avoid loading the supercampaign prefab as a resource

--- client/Assets/Scripts/CampaignsMap/SupercampaignsMapManager.cs
+++ client/Assets/Scripts/CampaignsMap/SupercampaignsMapManager.cs
@@ -1,23 +1,26 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class SupercampaignsMapManager : MonoBehaviour
 {
     [SerializeField]
     LevelManager sceneManager;
-    private GameObject supercampaignPrefab;
+
+    [SerializeField]
+    AddressableInstantiator addressableInstantiator;
+
+    private GameObject supercampaignInstance;
     public static string selectedSuperCampaignName;
 
-    void Start()
+    async void Start()
     {
-        GameObject selectedPrefab = Resources.Load("Supercampaigns/" + selectedSuperCampaignName) as GameObject;
-        supercampaignPrefab = Instantiate(selectedPrefab, new Vector3(0, 0, 0), Quaternion.identity);
-
+        await InstantiateSupercampaign();
         SocketConnection.Instance.GetCampaigns(GlobalUserData.Instance.User.id, (campaigns) =>
         {
             // this needs to be refactored, the campaigns have two parallel "paths" that do different things, they should be unified into the static class
             LevelProgress.campaigns = campaigns;
-            List<CampaignItem> campaignItems = new List<CampaignItem>(supercampaignPrefab.GetComponentsInChildren<CampaignItem>());
+            List<CampaignItem> campaignItems = new List<CampaignItem>(supercampaignInstance.GetComponentsInChildren<CampaignItem>());
             GenerateCampaigns(campaigns, campaignItems);
         });
     }
@@ -28,6 +31,18 @@ public class SupercampaignsMapManager : MonoBehaviour
         {
             campaignItems[campaignsIndex].sceneManager = sceneManager;
             campaignItems[campaignsIndex].SetCampaignData(campaigns[campaignsIndex]);
+        }
+    }
+
+    private async Task InstantiateSupercampaign()
+    {
+        if (selectedSuperCampaignName == "Main")
+        {
+            supercampaignInstance = await addressableInstantiator.InstantiateMainSupercampaign();
+        }
+        else
+        {
+            Debug.LogError("Supercampaign not found: " + selectedSuperCampaignName);
         }
     }
 }
