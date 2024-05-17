a1a25018625671adcf478ad1cf64fbb8a565e8a8
Theo <theokatz@gmail.com>
2024-05-17 10:11:39 -0300

    fix formatting

--- client/Assets/Scripts/Shared/SceneNavigator.cs
+++ client/Assets/Scripts/Shared/SceneNavigator.cs
@@ -5,42 +5,52 @@ using UnityEngine.SceneManagement;
 
 public class SceneNavigator : MonoBehaviour
 {
-    public static Stack<string> sceneHistory = new Stack<string>();
+	public static Stack<string> sceneHistory = new Stack<string>();
 
-	void Update() {
-		if(Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer) {
-			if(Input.GetKey(KeyCode.Escape)) {
-				BackToPreviousScene	();
+	void Update()
+	{
+		if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+		{
+			if (Input.GetKey(KeyCode.Escape))
+			{
+				BackToPreviousScene();
 			}
 		}
 	}
 
-    public void ChangeToScene(string targetSceneName) {
+	public void ChangeToScene(string targetSceneName)
+	{
 
-		if(sceneHistory.Contains(targetSceneName)) {
-			while(targetSceneName != sceneHistory.Peek()) {
+		if (sceneHistory.Contains(targetSceneName))
+		{
+			while (targetSceneName != sceneHistory.Peek())
+			{
 				sceneHistory.Pop();
 			}
 			sceneHistory.Pop();
 		}
-		else {
-			sceneHistory.Push(SceneManager.GetActiveScene().name); 
+		else
+		{
+			sceneHistory.Push(SceneManager.GetActiveScene().name);
 		}
-		
-        SceneManager.LoadScene(targetSceneName);
-    }
 
-	public void BackToPreviousScene() {
+		SceneManager.LoadScene(targetSceneName);
+	}
+
+	public void BackToPreviousScene()
+	{
 		SceneManager.LoadScene(sceneHistory.Pop());
 	}
 
-    // This is a workaround to avoid killing the sound effect when changing scenes.
-    public void ChangeToSceneWithDelay(string sceneName) {
-        StartCoroutine(ChangeToSceneAfterSeconds(sceneName, 0.1f));
-    }
+	// This is a workaround to avoid killing the sound effect when changing scenes.
+	public void ChangeToSceneWithDelay(string sceneName)
+	{
+		StartCoroutine(ChangeToSceneAfterSeconds(sceneName, 0.1f));
+	}
 
-    public IEnumerator ChangeToSceneAfterSeconds(string sceneName, float seconds) {
-        yield return new WaitForSeconds(seconds);
-        ChangeToScene(sceneName);
-    }
+	public IEnumerator ChangeToSceneAfterSeconds(string sceneName, float seconds)
+	{
+		yield return new WaitForSeconds(seconds);
+		ChangeToScene(sceneName);
+	}
 }
