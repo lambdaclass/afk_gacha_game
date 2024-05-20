dc5999918fea0aca1532b9a7dc9800d5f7051071
NContinanza <nicolas.continanza@lambdaclass.com>
2024-05-20 18:10:58 -0300

    Remove unused script

--- client/Assets/Scripts/Supercampaign.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Supercampaign", menuName = "Supercampaigns")]
-public class Supercampaign : ScriptableObject
-{
-    public new string name;
-    public Sprite background;
-}
