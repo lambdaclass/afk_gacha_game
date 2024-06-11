ca3b7b45305195ef1015c2c3aa7849d1864e19f3
lotuuu <nicolas@lotufo.com.ar>
2024-06-11 18:32:23 -0300

    Format code

--- client/Assets/Scripts/Campaign/ItemReward.cs
+++ client/Assets/Scripts/Campaign/ItemReward.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-
 public class ItemReward
 {
     public ItemTemplate itemTemplate;
--- client/Assets/Scripts/Campaign/UnitReward.cs
+++ client/Assets/Scripts/Campaign/UnitReward.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-
 public class UnitReward
 {
     public Character character;
